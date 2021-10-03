edf36fc0f0be03a586b9c292163ca306b8bbc300
Nguyen Minh Duc <86579678+ducnguyenedu@users.noreply.github.com>
2021-10-03 09:06:53 +0700

    commit

---
+++ src/BetweenTwoSets/Solution.java
@@ -0,0 +1,79 @@
+package BetweenTwoSets;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        // Range of int goes from the LAST element of A to the FIRST of B
+        int start = a.get(a.size() - 1), end = b.get(0);
+        int count = 0;
+        for (int x = start; x <= end; x++) {
+            Boolean a_pass = true, b_pass = true;
+            for (Integer ai : a
+            ) {
+                if (x % ai != 0) {
+
+                    a_pass = false;
+                    break;
+                }
+            }
+            if (!a_pass) continue;
+            for (Integer bi : b
+            ) {
+                if (bi % x != 0) {
+                    // Idem A loop
+                    b_pass = false;
+                    break;
+                }
+            }
+            if (a_pass && b_pass) count++;
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int total = Result.getTotalX(arr, brr);
+
+        bufferedWriter.write(String.valueOf(total));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
--- src/ReverseALinkedList/Solution.java
+++ src/ReverseALinkedList/Solution.java
@@ -1,12 +1,60 @@
 package ReverseALinkedList;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    public static SinglyLinkedListNode reverse(SinglyLinkedListNode llist) {
+        // Write your code here
+        if (llist == null || llist.next == null) {
+            return llist;
+        }
+        SinglyLinkedListNode remaining = reverse(llist.next);
+        llist.next.next = llist;
+        llist.next = null;
+        return remaining;
+    }
+
+    //class Result {
+
+    /*
+     * Complete the 'reverse' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    //}
 
     static class SinglyLinkedListNode {
         public int data;
@@ -38,56 +86,6 @@ public class Solution {
 
             this.tail = node;
         }
-    }
-
-    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
-        while (node != null) {
-            bufferedWriter.write(String.valueOf(node.data));
-
-            node = node.next;
-
-            if (node != null) {
-                bufferedWriter.write(sep);
-            }
-        }
-    }
-
-    //class Result {
-
-    /*
-     * Complete the 'reverse' function below.
-     *
-     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
-     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
-     */
-
-    /*
-     * For your reference:
-     *
-     * SinglyLinkedListNode {
-     *     int data;
-     *     SinglyLinkedListNode next;
-     * }
-     *
-     */
-
-    public static SinglyLinkedListNode reverse(SinglyLinkedListNode llist) {
-        // Write your code here
-        if (llist == null || llist.next == null) {
-            return llist;
-        }
-        SinglyLinkedListNode remaining = reverse(llist.next);
-        llist.next.next = llist;
-        llist.next = null;
-        return remaining;
-    }
-
-    //}
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-
     }
 }
 
