b56451d5830c1bb46c310d056b56c7ee85e9b727
ducnguyenedu <duc.education.nguyen@gmail.com>
2022-03-16 09:37:45 +0700

    commit

--- Java_Code/src/ConnectingTowns/Solution.java
+++ Java_Code/src/ConnectingTowns/Solution.java
@@ -1,4 +1,55 @@
 package ConnectingTowns;
 
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'connectingTowns' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY routes
+     */
+
+    public static int connectingTowns(int n, List<Integer> routes) {
+        // Write your code here
+        return 0;
+    }
+
+}
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> routes = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                int result = Result.connectingTowns(n, routes);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
