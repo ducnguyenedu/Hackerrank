8a3b66f5586c221303cbe0283815b9190dc85a3d
Nguyễn Minh Đức <108848660+hideongloomist@users.noreply.github.com>
2022-10-04 05:40:48 +0700

    https://www.hackerrank.com/challenges/arrays-introduction/problem

---
+++ c++/arrays_introduction/solution.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int array_length;
+    cin >> array_length;
+    int arr[array_length];
+    for(int i=0;i<array_length;i++){
+        int input;
+        cin >> input;
+        arr[i] = input;
+    }
+
+    for(int i=array_length;i>0;i--){
+        int output;
+        output = arr[i-1];
+        cout << output << ' ';
+    }
+
+    return 0;
+}
